5b1930e716a6f51f30ac424324f7042a2fd936fb
samtomcom <sam.comer@gmail.com>
2020-04-18 14:28:08 +0100

    working on parameters

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 import os
 import locale
+import argparse
 
 from datetime import datetime
 from datetime import timedelta
@@ -16,59 +17,103 @@ N = rough percentage of CPU to use, 4 to use 50% with 8 threads, 2 for 25%
 3rd argument is empty for verbose, anything else for quiet mode
 """
 
+## Start
+
 start = datetime.now()
 
-if len(sys.argv) == 2: # 1 param = path
-	f = path.normpath(sys.argv[1])
-	threads = "4"
-else: # more = threads, path, [q]
-	threads = sys.argv[1]
-	f = path.normpath(sys.argv[2])
+## Get parameters
 
-if not path.exists(f):
-	raise IOError('Given file does not exist')
+parser = argparse.ArgumentParser(description='Test argparsing.')
+parser.add_argument('-f', '--file', help='file to process')
+parser.add_argument('-d', '--dir', help='directory of files to process')
+parser.add_argument('-l', '--list', help='file containing list of files to process')
+parser.add_argument('-p', '--cpu', help='percentage of cpu to use, will be rounded')
 
-dirn, base = path.split(f)
-name, ext = path.splitext(base)
+parser.add_argument('-q', '--quiet', nargs='?', help='quiet mode, don\'t show ffmpeg output',
+					default=False)
 
-newf = path.join(dirn, name + "_" + ext)
 
-loglevel = "info"
-if len(sys.argv) == 4:
-	loglevel = "warning"
+args = parser.parse_args()
+print(args)
 
-subprocess.run(["ffmpeg", 
-	"-i", f, 
-	"-c:v", "libx265",
-	"-c:a", "copy", 
-	"-ac", "2", 
-	"-threads", threads, 
-	"-hide_banner", 
-	"-loglevel", loglevel, 
-	newf
-])
+## Normalise parameters
 
-oldsize = int(os.path.getsize(f) / 1024.0)
-newsize = int(os.path.getsize(newf) / 1024.0)
-delta = (oldsize-newsize)/oldsize*100
 
-locale.setlocale(locale.LC_ALL, 'en_GB')
+## Run re-encode
 
-print('\nRe-encoded "{}"'.format(base))
-print("Reduced by {:.2f}%".format(delta))
-
-print(locale.format_string("\nOld Size: %12d KiB", oldsize, grouping=True))
-print(locale.format_string("New Size: %12d KiB", newsize, grouping=True))
-
-if oldsize > newsize:
-	os.remove(f)
-	os.rename(newf, f)
-	print("File replaced")
-else:
-	os.remove(newf)
-	print("File not replaced")
+## Clean up
 
 end = datetime.now()
 end_time = end.strftime("%H:%M:%S")
 duration = (end-start)
 print("\nFinished at", end_time, "taking", duration, "\n")
+
+
+
+
+
+
+
+
+
+
+
+
+#################################
+
+# start = datetime.now()
+
+# if len(sys.argv) == 2: # 1 param = path
+# 	f = path.normpath(sys.argv[1])
+# 	threads = "4"
+# else: # more = threads, path, [q]
+# 	threads = sys.argv[1]
+# 	f = path.normpath(sys.argv[2])
+
+# if not path.exists(f):
+# 	raise IOError('Given file does not exist')
+
+# dirn, base = path.split(f)
+# name, ext = path.splitext(base)
+
+# newf = path.join(dirn, name + "_" + ext)
+
+# loglevel = "info"
+# if len(sys.argv) == 4:
+# 	loglevel = "warning"
+
+# subprocess.run(["ffmpeg", 
+# 	"-i", f, 
+# 	"-c:v", "libx265",
+# 	"-c:a", "copy", 
+# 	"-ac", "2", 
+# 	"-threads", threads, 
+# 	"-hide_banner", 
+# 	"-loglevel", loglevel, 
+# 	newf
+# ])
+
+# oldsize = int(os.path.getsize(f) / 1024.0)
+# newsize = int(os.path.getsize(newf) / 1024.0)
+# delta = (oldsize-newsize)/oldsize*100
+
+# locale.setlocale(locale.LC_ALL, 'en_GB')
+
+# print('\nRe-encoded "{}"'.format(base))
+# print("Reduced by {:.2f}%".format(delta))
+
+# print(locale.format_string("\nOld Size: %12d KiB", oldsize, grouping=True))
+# print(locale.format_string("New Size: %12d KiB", newsize, grouping=True))
+
+# if oldsize > newsize:
+# 	os.remove(f)
+# 	os.rename(newf, f)
+# 	print("File replaced")
+# else:
+# 	os.remove(newf)
+# 	print("File not replaced")
+
+# end = datetime.now()
+# end_time = end.strftime("%H:%M:%S")
+# duration = (end-start)
+# print("\nFinished at", end_time, "taking", duration, "\n")
